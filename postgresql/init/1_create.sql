408e1212b92e2a0450733b179260177a84874b95
tokizuoh <tokizo819@gmail.com>
2022-01-15 17:08:47 +0900

    PosgreSQLコンテナを起動する際にcsvからテーブルを作成する処理を追加

---
+++ postgresql/init/1_create.sql
@@ -0,0 +1,13 @@
+create table idol
+(
+    id integer not null, 
+    name varchar not null,
+    age integer not null,
+    height integer not null,
+    birth_place varchar not null,
+    birth_day varchar not null,
+    blood_type varchar not null,
+    unit varchar,
+    PRIMARY KEY (id)
+);
+comment on table idol is 'idol_list.csv';
---
+++ postgresql/init/2_copy.sql
@@ -0,0 +1,4 @@
+copy idol(id,name,age,height,birth_place,birth_day,blood_type,unit)
+  from '/docker-entrypoint-initdb.d/idol_list.csv' with csv header;
+
+  
