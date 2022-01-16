dc0260ab7f1a1505f297aa1e1987e597541937b6
tokizuoh <tokizo819@gmail.com>
2022-01-16 14:52:37 +0900

    DBからデータを取得する処理を追加

--- server/main.go
+++ server/main.go
@@ -21,6 +21,17 @@ func getDataSourceNameString(dsn datasourceName) string {
 	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", dsn.host, dsn.port, dsn.user, dsn.password, dsn.dbName, dsn.sslMode)
 }
 
+type idol struct {
+	id          string
+	name        string
+	age         int
+	height      int
+	birth_place string
+	birth_day   string
+	blood_type  string
+	unit        string
+}
+
 func main() {
 	dsn := datasourceName{
 		host:     "127.0.0.1",
@@ -38,5 +49,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// TODO: PostgreSQLからデータを取得する処理を追加する
+	rows, err := db.Query("SELECT * FROM idol")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var idols []idol
+	for rows.Next() {
+		var i idol
+		rows.Scan(&i.id, &i.name, &i.age, &i.height, &i.birth_place, &i.birth_day, &i.blood_type, &i.unit)
+		idols = append(idols, i)
+	}
 }
