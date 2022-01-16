6a819afb9194b9cb923984b898e6ee306979eb5f
tokizuoh <tokizo819@gmail.com>
2022-01-16 14:17:00 +0900

    GoからPostgreSQLに接続する処理を追加

--- server/main.go
+++ server/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	log.Println("Hello, World!")
+	db, err := sql.Open("postgres", "host=127.0.0.1 port=5423 user=postgres password=postgres dbname=postgres")
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// TODO: PostgreSQLからデータを取得する処理を追加する
 }
