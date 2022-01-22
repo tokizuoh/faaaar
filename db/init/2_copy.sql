04160207ff7c2b7a5812147d291dfe54caf2ced7
tokizuoh <tokizo819@gmail.com>
2022-01-22 13:14:35 +0900

    [#8] CSVからidols列を削除したことによるSQLファイルの修正対応

--- db/init/2_copy.sql
+++ db/init/2_copy.sql
@@ -1,5 +1,5 @@
 copy idol(id,name,age,height,birth_place,birth_day,blood_type,unit)
   from '/docker-entrypoint-initdb.d/idol_list.csv' with csv header;
 
-copy unit(id,name,idols)
+copy unit(id,name)
   from '/docker-entrypoint-initdb.d/unit_list.csv' with csv header;
