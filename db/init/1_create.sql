f7dd58442f669512742e1495d0ff26563625c951
tokizuoh <tokizo819@gmail.com>
2022-02-12 19:59:55 +0900

    [#53] unit, idol_unitテーブル作成時のカラムの設定などをMySQL仕様に変更

--- db/init/1_create.sql
+++ db/init/1_create.sql
@@ -4,30 +4,27 @@ USE shiny_colors_db;
 /* from idol_list.csv */
 CREATE TABLE idol
 (
-    id INT(2),
-    name VARCHAR(30),
-    latin_alphabet VARCHAR(30),
-    age INT(2),
-    height INT(3),
-    birth_place VARCHAR(30),
-    birth_day VARCHAR(5),
-    blood_type VARCHAR(2),
-    PRIMARY KEY (id)
+    `id` INT(2) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(30),
+    `latin_alphabet` VARCHAR(30),
+    `age` INT(2),
+    `height` INT(3),
+    `birth_place` VARCHAR(30),
+    `birth_day` VARCHAR(5),
+    `blood_type` VARCHAR(2)
 );
 
-create table unit
+/* from unit_list.csv */
+CREATE TABLE unit
 (
-    id varchar not null,
-    name varchar not null,
-    PRIMARY KEY (id)
+    `id` INT(3) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(30)
 );
-comment on table idol is 'unit_list.csv';
 
-create table idol_unit
+/* from idol_unit.csv */
+CREATE TABLE idol_unit
 (
-    id integer not null,
-    idol integer not null,
-    unit varchar not null,
-    PRIMARY KEY (id)
+    `id` INT(3) NOT NULL PRIMARY KEY,
+    `idol` VARCHAR(30),
+    `unit` VARCHAR(30)
 );
-comment on table idol is 'idol_unit.csv';
