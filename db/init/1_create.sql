5e8380ad7368915f29f72124704f7af101c8386c
tokizuoh <tokizo819@gmail.com>
2022-02-18 15:43:06 +0900

    [#56] カラムにNOT NULLを追加

--- db/init/1_create.sql
+++ db/init/1_create.sql
@@ -5,26 +5,26 @@ USE shiny_colors_db;
 CREATE TABLE idol
 (
     `id` INT(2) NOT NULL PRIMARY KEY,
-    `name` VARCHAR(30),
-    `latin_alphabet` VARCHAR(30),
-    `age` INT(2),
-    `height` INT(3),
-    `birth_place` VARCHAR(30),
-    `birth_day` VARCHAR(5),
-    `blood_type` VARCHAR(2)
+    `name` VARCHAR(30) NOT NULL,
+    `latin_alphabet` VARCHAR(30) NOT NULL,
+    `age` INT(2) NOT NULL,
+    `height` INT(3) NOT NULL,
+    `birth_place` VARCHAR(30) NOT NULL,
+    `birth_day` VARCHAR(5) NOT NULL,
+    `blood_type` VARCHAR(2) NOT NULL
 );
 
 /* from unit_list.csv */
 CREATE TABLE unit
 (
     `id` VARCHAR(10) NOT NULL PRIMARY KEY,
-    `name` VARCHAR(30)
+    `name` VARCHAR(30) NOT NULL
 );
 
 /* from idol_unit.csv */
 CREATE TABLE idol_unit
 (
     `id` INT(3) NOT NULL PRIMARY KEY,
-    `idol` VARCHAR(30),
-    `unit` VARCHAR(30)
+    `idol` VARCHAR(30) NOT NULL,
+    `unit` VARCHAR(30) NOT NULL
 );
