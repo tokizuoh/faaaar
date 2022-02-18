e0bb84b5fb2d09c75a79c317097f376af799508d
tokizuoh <tokizo819@gmail.com>
2022-02-18 21:19:34 +0900

    [#43] idolIdの有無で使用するresolverを変える処理を追加

--- server/fields/units_field.go
+++ server/fields/units_field.go
@@ -13,11 +13,19 @@ var UnitsField = &graphql.Field{
 	Type: graphql.NewList(types.UnitType),
 	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 		idolId, _ := p.Args["idolId"].(int)
-		result, err := resolvers.GetUnitsByIdolID(idolId)
-		if err != nil {
-			return nil, err
+		if idolId != 0 {
+			result, err := resolvers.GetUnitsByIdolID(idolId)
+			if err != nil {
+				return nil, err
+			}
+			return result, nil
+		} else {
+			result, err := resolvers.GetUnits()
+			if err != nil {
+				return nil, err
+			}
+			return result, nil
 		}
-		return result, nil
 	},
 	Args: graphql.FieldConfigArgument{
 		"idolId": &graphql.ArgumentConfig{
