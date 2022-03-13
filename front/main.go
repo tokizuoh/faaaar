95b71224cb3ae8d7d0a2d0f17354c7d0c12202d4
tokizuoh <tokizo819@gmail.com>
2022-03-13 11:02:31 +0900

    [#78] basic認証の実装を追加

--- front/main.go
+++ front/main.go
@@ -6,8 +6,48 @@ import (
 	"net/http"
 )
 
+// TODO: .envに入れる
+const basicAuthUsername = "root"
+const basicAuthPassword = "root"
+
+// TODO: ユーザーごとに認証したか判別する
+var doneAuth = false
+
+func basicAuth(r *http.Request) bool {
+	username, password, ok := r.BasicAuth()
+	if ok == false {
+		return false
+	}
+
+	if username != basicAuthUsername || password != basicAuthPassword {
+		return false
+	}
+
+	doneAuth = true
+	return true
+}
+
 func main() {
+	// Basic Authentication
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		if basicAuth(r) {
+			http.Redirect(rw, r, "/playground", http.StatusMovedPermanently)
+			return
+		} else {
+			rw.Header().Set("WWW-Authenticate", "Basic")
+			rw.WriteHeader(http.StatusUnauthorized)
+			http.Error(rw, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+	})
+
+	// GraphQL Playground
 	http.HandleFunc("/playground", func(rw http.ResponseWriter, r *http.Request) {
+		if doneAuth != true {
+			http.Redirect(rw, r, "/", http.StatusSeeOther)
+			return
+		}
+
 		t, err := template.ParseFiles("index.html")
 		if err != nil {
 			log.Fatal(err)
@@ -17,5 +57,6 @@ func main() {
 			log.Fatal(err)
 		}
 	})
+
 	http.ListenAndServe(":8081", nil)
 }
